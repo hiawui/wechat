b417faaa5567a3ad3d33710a4fb4f272645a9987
xiaoweilin <xiaoweilin@lifang-mbp.local>
2015-01-27 23:24:17 +0800

    update

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var configure = require('./configure');
+var loggerFactory = require('./middlewares/loggerFactory');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -14,16 +15,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(loggerFactory(configure.log));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
